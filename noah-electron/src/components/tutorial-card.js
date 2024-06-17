35494cdf2bdc0370e338ccebe8e85aa57c01f907
eli-johnson-04 <ed.j53190@gmail.com>
2024-06-17 16:17:29 +0000

    Decent styling for tutorials
    Tutorials have been well-styled, although more changes may be made in the future. The "Close Tutorial" button was also changed to "X" and moved to the top right of the modal.

--- noah-electron/src/components/tutorial-card.js
+++ noah-electron/src/components/tutorial-card.js
@@ -17,12 +17,14 @@ const TutorialCard = ({ color, label, sections }) => {
             </button>
 
             {/* Define the modal that will display tutorial information */}
-            <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}className="fixed inset-0 flex items-center justify-center p-4" style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' } }}>
-                <div className="bg-gray-300 overflow-auto rounded-lg shadow-lg" style={{ width: '85%', height: '80%', borderRadius: '13pt', backgroundColor: '#d9d9d9', borderColor: 'black', borderWidth: '5px', borderStyle: 'solid', padding: '20px'}}>
-                    <div style={{borderRadius: '13pt', borderColor: color, borderWidth: '5px', borderStyle: 'solid'}}>
-                    <h1 className='font-kanit text-3xl font-medium my-2'>{label}</h1>
-                    <div classname="font-kanit">{sections}</div>
-                        <button className="p-2 text-white bg-blue-500 hover:bg-blue-700 rounded" onClick={() => setIsModalOpen(false)}>Close Tutorial</button>
+            <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} className="fixed flex items-center justify-center p-4 inset-0" style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' } }} shouldCloseOnOverlayClick={true}>
+                <div className="flex w-4/5 h-4/5 bg-gray-300 overflow-auto rounded-lg shadow-lg p-5" style={{ borderRadius: '13pt', backgroundColor: '#d9d9d9', borderColor: 'black', borderWidth: '5px', borderStyle: 'solid' }}>
+                    <div className="flex flex-grow flex-col overflow-auto rounded-lg" style={{ borderRadius: '13pt', borderColor: color, borderWidth: '5px', borderStyle: 'solid', padding: '20px' }}>
+                        <div className="flex justify-between items-center">
+                            <h1 className='font-kanit text-3xl font-medium my-2'>{label}</h1>
+                            <button style={{ position: 'relative', backgroundColor: color }} className="ml-4 p-1 font-kanit text-3xl font-semibold text-black rounded border-solid border-2 border-black shadow-md hover:shadow-lg hover:-translate-y-1 ease-in-out" onClick={() => setIsModalOpen(false)}>X</button>
+                        </div>
+                        <div className="font-kanit">{sections}</div>
                     </div>
                 </div>
             </Modal>
